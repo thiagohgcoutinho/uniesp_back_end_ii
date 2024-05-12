ec9505985d5039b50853244f2b12db933451b027
Thiago Coutinho <2022211510098@iesp.edu.br>
2024-05-12 18:29:48 -0300

    processnet_start

---
+++ processnet/src/main/java/com/nexus/processnet/ProcessNetApplication.java
@@ -0,0 +1,13 @@
+package com.nexus.processnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessNetApplication.class, args);
+	}
+
+}
---
+++ processnet/src/test/java/com/nexus/processnet/ProcessNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexus.processnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
